5d6791edcee80ea4b4b58b5f4f499eb652b1b1bd
TahedoranIX <aleix.valdivieso@gmail.com>
2025-12-21 01:22:56 +0100

    fixed 1mm offset Y

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1740,7 +1740,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -6
-#define Y_MIN_POS -7
+#define Y_MIN_POS -6
 
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
